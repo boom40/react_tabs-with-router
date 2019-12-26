58dc8695cb8c7525d89661a5fa6315cf450afd83
boom40 <kickgames675@gmail.com>
2019-12-26 18:25:52 +0200

    Updates

---
+++ precache-manifest.d92ae6c5159cc6d67585168542699c63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd7cfd550cb216714377839f1a2e1e4",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "d7b577f961b77b2ced05",
+    "url": "/react_tabs-with-router/static/css/main.885f697a.chunk.css"
+  },
+  {
+    "revision": "a6ed9298720ddeba0193",
+    "url": "/react_tabs-with-router/static/js/2.605c7544.chunk.js"
+  },
+  {
+    "revision": "d7b577f961b77b2ced05",
+    "url": "/react_tabs-with-router/static/js/main.81b30439.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
--- src/HomePage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HomePage = () => (
-  <>
-    <h1 className="home">Home page</h1>
-  </>
-);
-
-export default HomePage;
